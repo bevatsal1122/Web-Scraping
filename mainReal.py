da78a739b73e3c5cfc9354144717f4eeb23cb401
Vatsal <bevatsal1122@gmail.com>
2022-08-09 22:56:04 +0530

    Updating mainReal.py

--- mainReal.py
+++ mainReal.py
@@ -15,18 +15,16 @@ def ignoreSpaces(text):
 htmlText = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(htmlText, 'lxml')
 ulTag = soup.find('ul', class_="new-joblist")
-liTags = ulTag.find_all('li')
+liTags = ulTag.find_all('li', class_="clearfix job-bx wht-shd-bx")
 for liTag in liTags:
-   titles = liTag.find_all('h2')
+   title = liTag.find('h2')
    companyName = liTag.find('h3')
    location = liTag.find('span')
    skills = liTag.find('span', class_="srp-skills")
-   for title in titles:
-      # print(title.text)
-      propName = ignoreSpaces(companyName.text)
-      propTitle = ignoreSpaces(title.text)
-      propSkills = ignoreSpaces(skills.text)
-      propLocation = ignoreSpaces(ignoreSpaces(location.text))
-      print()
-      print("Company Name: " + propName  + "\nJob Title: " + propTitle + "\nSkills Requirement: " + propSkills + "\nJob Location: " + propLocation);
-
+   # print(title.text)
+   propName = ignoreSpaces(companyName.text)
+   propTitle = ignoreSpaces(title.text)
+   propSkills = ignoreSpaces(skills.text)
+   propLocation = ignoreSpaces(location.text)
+   print('\n')
+   print("Company Name: " + propName  + "\nJob Title: " + propTitle + "\nSkills Requirement: " + propSkills + "\nJob Location: " + propLocation)
